0c2a1a612772536f8fe64e05853ae9fb2fb50486
Justin Turpin <ionstream@gmail.com>
2020-12-20 21:50:09 -0800

    turning off wal mode because its more of a pain than its worth

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ fn register_routes(app: &mut tide::Server<State>, config: &config::Config) {
 
 async fn bootstrap_database(config: &config::Config) -> tide::Result<SqlitePool> {
     let options = SqliteConnectOptions::from_str(config.database_url.as_str())?
-        .journal_mode(SqliteJournalMode::Wal)
+        .journal_mode(SqliteJournalMode::Off)
         .create_if_missing(true);
 
     let sqlite_pool = SqlitePool::connect_with(options).await?;
